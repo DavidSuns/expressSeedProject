c6775f90f3b3deeab04dc16e813485d283d2d05a
sunzhe <davidsunsz@hotmail.com>
2016-06-05 19:04:39 +0800

    update and delete article

--- routes/index.js
+++ routes/index.js
@@ -186,6 +186,52 @@ module.exports = function(router, app) {
     });
   });
 
+  router.route('/edit/:name/:day/:title')
+  .get(checkLogin)
+  .get(function (req, res) {
+    var currentUser = req.session.user;
+    Post.edit(currentUser.name, req.params.day, req.params.title, function (err, post) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('back');
+      }
+      res.render('edit', {
+        title: '编辑',
+        post: post,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  })
+  .post(checkLogin)
+  .post('/edit/:name/:day/:title', function (req, res) {
+    var currentUser = req.session.user;
+    Post.update(currentUser.name, req.params.day, req.params.title, req.body.post, function (err) {
+      var url = encodeURI('/u/' + req.params.name + '/' + req.params.day + '/' + req.params.title);
+      if (err) {
+        req.flash('error', err);
+        return res.redirect(url);
+      }
+      req.flash('success', '修改成功!');
+      res.redirect(url);
+    });
+  });
+
+  router.route('/remove/:name/:day/:title')
+  .get(checkLogin)
+  .get(function (req, res) {
+    var currentUser = req.session.user;
+    Post.remove(currentUser.name, req.params.day, req.params.title, function (err) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('back');
+      }
+      req.flash('success', '删除成功!');
+      res.redirect('/');
+    });
+  });
+
   router.route('*').get(function(request, res) {
     res.end("404!");
   });
