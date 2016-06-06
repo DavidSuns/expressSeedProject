3009dc49199ddc59b63d48207e7c41d3e831342e
sunzhe <davidsunsz@hotmail.com>
2016-06-06 22:36:17 +0800

    archive

--- routes/index.js
+++ routes/index.js
@@ -259,6 +259,22 @@ module.exports = function(router, app) {
     });
   });
 
+  router.route('/archive').get(function (req, res) {
+  Post.getArchive(function (err, posts) {
+    if (err) {
+      req.flash('error', err);
+      return res.redirect('/');
+    }
+    res.render('archive', {
+      title: '存档',
+      posts: posts,
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+});
+
   router.route('*').get(function(request, res) {
     res.end("404!");
   });
